96b018461fa2e656e30e29fca7700a691ce9c979
Leah Somerville <leah.somerville4@gmail.com>
2024-03-26 10:06:42 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Leah Somerville on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
